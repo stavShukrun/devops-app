c3b0c9a3b7b29098e11b563b6d4e5aa8b31e60da
Stav Shukrun <shukrunstav@gmail.com>
2022-07-12 01:24:33 +0300

    test

--- tests/UT.sh
+++ tests/UT.sh
@@ -2,7 +2,7 @@
 
 counter=0
 max_attempts=30
-until $(curl --output /dev/null --silent --head --fail $1:$2); do
+until $(curl --output /dev/null --silent --head --fail $1); do
     if [ ${counter} -eq ${max_attempts} ]; then
         echo "To many try, Uint Test failed"
         exit 1
